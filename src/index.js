541ec988e6997a6f48b507b6cc2c86bf13803345
cheeraboina srilakshmi <kongalasrilakshmi@gmail.com>
2023-11-10 14:47:16 +0530

    hiii

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import reportWebVitals from './reportWebVitals';
 // import Data from './Data';
 // import Update from './Update';
 // import Form from './Form';
-// import Usetstates from './Usetstates'
-import Onchange from './Onchange';
+import Usetstates from './Usetstates'
+// import Onchange from './Onchange';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -16,8 +16,8 @@ root.render(
     {/* <Data /> */}
     {/* <Update /> */}
     {/* <Form /> */}
-    {/* <Usetstates /> */}
-    <Onchange />
+    <Usetstates />
+    {/* <Onchange /> */}
   </React.StrictMode>
 );
 
